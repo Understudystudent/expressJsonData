fe3c45c7c9f3f29065ee65b37e571499d00fb593
Understudystudent <carlmjames2@gmail.com>
2024-02-12 16:24:31 +0200

    home time

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-console.log("hello world");
+import  Express  from "express";
+
+// Express app
+const app = express();
+
+// Router
+const router = express.Router()
+
+//Port
+const port = +process.env.PORT || 6000
+
+// Json Url
+
+const dataUrl = 'https://understudystudent.github.io/vueEOMPdata/data/'
+
+// Application Middleware
+app.use ( router)
+
+//  /=> home
+router.get('^/$|ejd', (req, res) => {
+    res.json ({
+        status: res.statusCode,
+        msg: "You're home"
+    })
+})
+
+// fetch all education
+
+router.get('/education',async (req, res) => {
+    let {education} = await (await fetch(dataUrl)).json();
+    res.json({
+        status: res.statusCode,
+        education
+    });
+    });
+
+app.listen(port, () => {
+    console.log(`Server is running on Port ${port}`);
+    });
